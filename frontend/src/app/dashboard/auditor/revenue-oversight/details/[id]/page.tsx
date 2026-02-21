6a3254f3a83cd36935bc56af83c25db4984b6bf6
allan-too <allansamuel571@gmail.com>
2026-02-21 17:20:02 +0300

    A

--- frontend/src/app/dashboard/auditor/revenue-oversight/details/[id]/page.tsx
+++ frontend/src/app/dashboard/auditor/revenue-oversight/details/[id]/page.tsx
@@ -1,6 +1,6 @@
-import dynamic from 'next/dynamic';
+import nextDynamic from 'next/dynamic';
 
-const PageContent = dynamic(() => import('./PageContent'), { ssr: false });
+const PageContent = nextDynamic(() => import('./PageContent'), { ssr: false });
 
 // This page uses dynamic routing and should not be statically generated
 export const dynamic = 'force-dynamic';
