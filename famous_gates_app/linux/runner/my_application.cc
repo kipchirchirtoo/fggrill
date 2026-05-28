e964223aef1631efd53daac826fc245ab2eb7e03
allan-too <allansamuel571@gmail.com>
2026-05-29 00:09:30 +0300

    chore: use Famous Gates app icon

--- famous_gates_app/linux/runner/my_application.cc
+++ famous_gates_app/linux/runner/my_application.cc
@@ -24,6 +24,12 @@ static void my_application_activate(GApplication* application) {
   MyApplication* self = MY_APPLICATION(application);
   GtkWindow* window =
       GTK_WINDOW(gtk_application_window_new(GTK_APPLICATION(application)));
+  g_autoptr(GError) icon_error = nullptr;
+  if (!gtk_window_set_icon_from_file(
+          window, "data/flutter_assets/assets/frontend_public/fglogo.png",
+          &icon_error)) {
+    g_clear_error(&icon_error);
+  }
 
   // Use a header bar when running in GNOME as this is the common style used
   // by applications and is the setup most users will be using (e.g. Ubuntu
